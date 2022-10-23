28baae1e7cb8ffeb9c5d205a8cc6a8d18f3fad98
= <=>
2022-10-23 17:03:40 +0800

    'final'

--- utils/components/PageTitle.js
+++ utils/components/PageTitle.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default function PageTitle({page}){
  
@@ -6,7 +7,7 @@ export default function PageTitle({page}){
     <div className="container clearfix">
         <h1>{page.title}</h1>
         <ol className="breadcrumb">
-            <li className="breadcrumb-item"><a href="#">Home</a></li>
+            <li className="breadcrumb-item"><Link href="/"><a>Home</a></Link></li>
             <li className="breadcrumb-item active" aria-current="page">{page.title}</li>
         </ol>
     </div>
